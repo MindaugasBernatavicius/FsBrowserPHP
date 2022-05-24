d2a885c2af7a201b1772a8025b0ecdd04d70b599
M1ndas <darbas.mindaugas@gmail.com>
2022-05-24 09:39:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     </style>
     <body>
         <?php 
-            $path = './' . $_GET["path"]; // "path" = "./dir1/dir/
+            $path = isset($_GET["path"]) ? './' . $_GET["path"] : './' ;
             $files_and_dirs = scandir($path);
 
             print('<h2>Directory contents: ' . str_replace('?path=/','',$_SERVER['REQUEST_URI']) . '</h2>');

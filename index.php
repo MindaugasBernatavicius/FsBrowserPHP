6722671e947ca1d74c2bf5ebec1f66e3f5d33d77
M1ndas <darbas.mindaugas@gmail.com>
2022-05-24 09:39:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
                 die();
             }
 
-            $path = './' . $_GET["path"];
+            $path = isset($_GET["path"]) ? './' . $_GET["path"] : './' ;
             $files_and_dirs = scandir($path);
 
             print('<h2>Directory contents: ' . str_replace('?path=/','',$_SERVER['REQUEST_URI']) . '</h2>');
